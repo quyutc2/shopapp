58f6674efcd63893c750f66cb93fd84846e365fc
quyutc2 <trongquy1628@gmail.com>
2024-04-27 20:14:57 +0700

    list product

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
-  bootstrap: [AppComponent],
+  imports: [AppModule, ServerModule],
+  bootstrap: [HomeComponent],
 })
 export class AppServerModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [HomeComponent],
+  imports: [BrowserModule],
+  providers: [provideClientHydration()],
+  bootstrap: [HomeComponent],
 })
-export class AppModule { }
+export class AppModule {}
