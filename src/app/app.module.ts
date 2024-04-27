117745f3f75bb9f90d74fdf2a114177b4b7680da
quyutc2 <trongquy1628@gmail.com>
2024-04-27 21:32:57 +0700

    header, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import {
   provideClientHydration,
 } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [provideClientHydration()],
   bootstrap: [HomeComponent],
