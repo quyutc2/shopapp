5677435922f8812f1223b33e0b62b4329d6fad0c
quyutc2 <trongquy1628@gmail.com>
2024-06-04 19:34:00 +0700

    data binding 2 way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,7 @@ import { RegisterComponent } from './register/register.component';
     LoginComponent,
     RegisterComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [provideClientHydration()],
   bootstrap: [
     //HomeComponent,
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  phone: string;
+  password: string;
+  retypePassword: string;
+  fullName: string;
+  address: string;
+  isAccepted: boolean;
+  dateOfBirth: Date;
 
+  constructor() {
+    this.phone = '';
+    this.password = '';
+    this.retypePassword = '';
+    this.fullName = '';
+    this.address = '';
+    this.isAccepted = false;
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+  }
+  onPhoneChange() {
+    console.log('Phone typed: ${this.phone}');
+  }
+  register() {
+    const message =
+      `phone: ${this.phone}` +
+      `password: ${this.password}` +
+      `retypePassword: ${this.retypePassword}` +
+      `address: ${this.address}` +
+      `fullName: ${this.fullName}` +
+      `isAccepted: ${this.isAccepted}` +
+      `dateOfBirth: ${this.dateOfBirth}`;
+    alert(message);
+  }
 }
